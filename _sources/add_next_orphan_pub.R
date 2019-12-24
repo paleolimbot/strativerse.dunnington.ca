7c4e809a270328bf0288a09ca65a464e21992f7c
Dewey Dunnington <dewey@fishandwhistle.net>
2019-12-24 09:38:43 -0400

    save orphan pub file

---
+++ _sources/add_next_orphan_pub.R
@@ -0,0 +1,16 @@
+
+pubs <- sv_publications()
+recs <- sv_records()
+
+pubs_with_records <- do.call(c, recs$publications) %>% 
+  unique()
+
+pubs_skip <- c("williams80", "brugam_carlson81")
+
+pubs %>% 
+  filter(!(slug %in% c(pubs_with_records, pubs_skip))) %>% 
+  arrange(date) %>% 
+  slice(1) %>% 
+  pull(slug) %>% 
+  paste0("publication/", .) %>% 
+  sv_browse()
