6513242d450362d5524de2c22381e2dfa6c7ac72
Dewey Dunnington <dewey@fishandwhistle.net>
2020-01-05 20:42:48 -0400

    update list of skipped pubs

--- _sources/add_next_orphan_pub.R
+++ _sources/add_next_orphan_pub.R
@@ -5,7 +5,7 @@ recs <- sv_records()
 pubs_with_records <- do.call(c, recs$publications) %>% 
   unique()
 
-pubs_skip <- c("brugam_carlson81", "johansson85")
+pubs_skip <- c("brugam_carlson81", "johansson85", "benoit_etal94")
 
 orphan_pubs <- pubs %>% 
   filter(!(slug %in% c(pubs_with_records, pubs_skip)))
